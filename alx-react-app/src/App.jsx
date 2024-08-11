c27c4d7b0bc4148c0e95548f57534472cccaaaf6
Paul Mwanzia <paulmwanzia97@gmail.com>
2024-08-11 23:13:23 +0300

    component userprofile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,12 +6,14 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './Header'
 import MainContent from './MainContent'
 import Footer from './Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     <Header />
     <MainContent />
     <Footer />
