f2a9a1a1ec91fefb23c216eb997c2ee1aca93155
Paul Mwanzia <paulmwanzia97@gmail.com>
2024-08-18 19:48:28 +0300

    using props and hooks

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,22 +1,23 @@
-
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import WelcomeMessage from './components/WelcomeMessage'
 import './App.css'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
 import UserProfile from './components/UserProfile'
-import UserContext from './components/UserContext'
-import ProfilePage from './components/ProfilePage'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [userData, setData] = useState("")
 
   return (
     <>
+    <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    <Header />
+    <MainContent />
+    <Footer />
+    <WelcomeMessage />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -37,21 +38,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-
-      <WelcomeMessage />
-      <Header />
-      <MainContent />
-      <Footer />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-
-    {/* User Context */}
-      <UserContext.Provider value={userData}>
-          <ProfilePage />
-      </UserContext.Provider>
-
     </>
   )
 }
 
 export default App
-
