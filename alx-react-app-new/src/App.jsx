60317e4eaa1614c7ddd54860ef7e83f26c324f46
Paul Mwanzia <paulmwanzia97@gmail.com>
2024-08-18 20:04:43 +0300

    edits

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -8,12 +8,10 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
-import UserContext from './components/UserContext'
-import ProfilePage from './components/ProfilePage'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [userData, setData] = useState("")
 
   return (
     <>
@@ -43,12 +41,7 @@ function App() {
       <MainContent />
       <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-
-    {/* User Context */}
-      <UserContext.Provider value={userData}>
-          <ProfilePage />
-      </UserContext.Provider>
-
+      <Counter />
     </>
   )
 }
