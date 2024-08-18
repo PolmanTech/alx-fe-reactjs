a618f3cbc2dd9ecf9ec5a5dd568ca57ee0b9a18a
Paul Mwanzia <paulmwanzia97@gmail.com>
2024-08-18 19:32:14 +0300

    styling using jsx

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+    return (
+        <footer>
+            <p style={{fontWeight: 'bold', fontFamily: 'sans-serif'}} >&copy; 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
+
