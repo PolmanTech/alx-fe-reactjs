c23762cc47b7da679aa4d78a610a9b56f37c0830
Paul Mwanzia <paulmwanzia97@gmail.com>
2024-08-25 21:56:32 +0300

    error fix on app.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,18 +1,29 @@
 
+
 import './App.css'
-import AddRecipeForm from './components/AddRecipeForm'
-import RecipeDetails from './components/RecipeDetails'
-import RecipeList from './components/RecipeList'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
+import Recommendations from './components/RecommendationsList';
 
 function App() {
 
   return (
-    <>
-      <RecipeList />
-      <AddRecipeForm />
-      <RecipeDetails />
-    </>
+    
+      <Router>
+        <div className='main-container'>
+          <h1>ALX React Zustand - Recipe</h1>
+          <AddRecipeForm />
+          <Recommendations />
+          <Routes>
+            <Route path="/" element={<RecipeList />} />
+            <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+          </Routes>
+        </div>
+      </Router>
   )
 }
 
 export default App
+
