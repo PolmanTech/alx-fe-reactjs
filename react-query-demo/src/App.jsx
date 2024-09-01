4527b5e54e6614f6c1f627e1047279f0e4e44b3b
Paul Mwanzia <paulmwanzia97@gmail.com>
2024-09-01 15:53:12 +0300

    using react-query for data fetching

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,17 @@
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'
+import PostsComponent from './components/PostsComponent';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PostsComponent />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
