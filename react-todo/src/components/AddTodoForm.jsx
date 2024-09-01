36cc0ea022421fc9468a63edc12266370221fca1
Paul Mwanzia <paulmwanzia97@gmail.com>
2024-09-01 16:47:35 +0300

    todo list app

---
+++ react-todo/src/components/AddTodoForm.jsx
@@ -0,0 +1,18 @@
+
+/* eslint-disable react/prop-types */
+function AddTodoForm({ newTodo, setNewTodo, addTodo }) {
+    return (
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Add a new todo"
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+    );
+  }
+  
+  export default AddTodoForm;
+  
